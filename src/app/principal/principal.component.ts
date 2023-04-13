67f914afe166567cfc1f851c018faa92974ebf2c
leandro <arcanjo.lm@gmail.com>
2023-04-13 17:39:15 -0300

    adicionando a função selecionar

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -9,6 +11,27 @@ export class PrincipalComponent {
 
   // Váriavel para visibilidade dos botões
    btnCadastro: boolean= true;
+
+   //JSON de clientes
+   clientes:Cliente[] = [];
+
+   // Construtor
+   constructor(private servico:ClienteService){}
+
+   //Método de seleção
+   selecionar():void{
+      this.servico.selecionar()
+      .subscribe(retorno => this.clientes = retorno);
+
+   } 
+
+   // Método de inicialização
+   ngOnInit(){
+     this.selecionar();
+
+   }
+
+
   
 }
 
